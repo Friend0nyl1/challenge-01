133b0352efba48fc975152e916c53b3c197cced9
Friend0nyl1 <thanakit.work.lab@gmail.com>
2025-06-24 22:52:28 +0700

    Update getAllProducts use Pagination

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 import { db } from "../db";
 import { productStats, products } from "../db/schema/products";
 import { publicProcedure, router } from "../lib/trpc";
@@ -6,14 +7,34 @@ export const appRouter = router({
 	healthCheck: publicProcedure.query(() => {
 		return "OK";
 	}),
-	getAllProducts: publicProcedure.query(async () => {
-		const allProducts = await db.select().from(products).limit(50).offset(0);
-		await new Promise((resolve) => setTimeout(resolve, 3000));
+	getAllProducts: publicProcedure
+		.input(
+			z.object({
+				limit: z.number().optional(),
+				offset: z.number().optional(),
+			}),
+		)
+		.query(async (opts) => {
+			const offset = opts.input.offset || 1;
+			const limit = opts.input.limit || 50;
 
-		return {
-			products: allProducts,
-		};
-	}),
+			const totalItem = await db.$count(products);
+			const totalPage = Math.ceil(totalItem / limit);
+			const skip = (offset - 1) * limit;
+
+			const allProducts = await db
+				.select()
+				.from(products)
+				.limit(limit)
+				.offset(skip);
+			await new Promise((resolve) => setTimeout(resolve, 3000));
+
+			return {
+				products: allProducts,
+				totalPage: totalPage,
+				totalItem: totalItem,
+			};
+		}),
 	getLatestProductStats: publicProcedure.query(async () => {
 		const latestStats = await db
 			.select()
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { caller } from "@/lib/trpc/server";
 import { ProductsTable } from "./_components/table";
 
 export default async function Home() {
-	const { products } = await caller.getAllProducts();
+	const { products } = await caller.getAllProducts({ limit: 50, offset: 1 });
 	const { stats } = await caller.getLatestProductStats();
 
 	return (
