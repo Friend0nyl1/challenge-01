a12cbd0d4e45764b3584ea5db30d8b78b1c8013e
Friend0nyl1 <thanakit.work.lab@gmail.com>
2025-06-25 00:04:47 +0700

    Change offset-base to cursor-base on getAllProducts

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -2,6 +2,7 @@ import z from "zod";
 import { db } from "../db";
 import { productStats, products } from "../db/schema/products";
 import { publicProcedure, router } from "../lib/trpc";
+import { asc, desc, gt, lt } from "drizzle-orm";
 
 export const appRouter = router({
 	healthCheck: publicProcedure.query(() => {
@@ -10,29 +11,46 @@ export const appRouter = router({
 	getAllProducts: publicProcedure
 		.input(
 			z.object({
-				limit: z.number().optional(),
-				offset: z.number().optional(),
+				limit: z.number().min(1).max(100).optional().default(50),
+				cursor: z.number().nullish(),
+				direction: z.enum(['forward', 'backward']).optional().default('forward'),
 			}),
 		)
 		.query(async (opts) => {
-			const offset = opts.input.offset || 1;
-			const limit = opts.input.limit || 50;
-
-			const totalItem = await db.$count(products);
-			const totalPage = Math.ceil(totalItem / limit);
-			const skip = (offset - 1) * limit;
-
+			const { limit, cursor, direction } = opts.input;
 			const allProducts = await db
 				.select()
 				.from(products)
-				.limit(limit)
-				.offset(skip);
+				.where(direction === 'forward' ? gt(products.id, cursor || 0) : lt(products.id, cursor || 0))
+				.orderBy(direction === 'forward' ? asc(products.id) : desc(products.id))
+				.limit(limit);
+
+			let nextCursor: typeof cursor | undefined = undefined;
+			let previousCursor: typeof cursor | undefined = undefined;
+
+			if (allProducts.length > limit) {
+				if (direction === 'forward') {
+					const nextItem = allProducts.pop();
+					nextCursor = nextItem!.id as number
+				} else {
+					const previousItem = allProducts.shift()
+					previousCursor = previousItem!.id as number
+					nextCursor = allProducts[allProducts.length - 1].id as number
+				}
+			}
+
+			if(direction === 'backward'){
+				allProducts.reverse()
+			}
+
+
+
 			await new Promise((resolve) => setTimeout(resolve, 3000));
 
 			return {
 				products: allProducts,
-				totalPage: totalPage,
-				totalItem: totalItem,
+				nextCursor,
+				previousCursor
 			};
 		}),
 	getLatestProductStats: publicProcedure.query(async () => {
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,7 @@ import { caller } from "@/lib/trpc/server";
 import { ProductsTable } from "./_components/table";
 
 export default async function Home() {
-	const { products } = await caller.getAllProducts({ limit: 50, offset: 1 });
+	const { products } = await caller.getAllProducts({ limit: 50 , cursor: 1, direction: 'backward' });
 	const { stats } = await caller.getLatestProductStats();
 
 	return (
